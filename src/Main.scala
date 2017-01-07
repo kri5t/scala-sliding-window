48dcd02d821b6bf3061f3eb5529e7bf627f09565
Kristian Barrett <kbarrett-86@hotmail.com>
2017-01-07 19:22:05 +0100

    * Added gitignore * Added method to analyse the sliding window * Assuming sliding window of 60. Can be altered with main first arg.

---
+++ src/Main.scala
@@ -0,0 +1,48 @@
+import scala.io.Source
+
+/**
+  * Created by kristian on 07/01/2017.
+  */
+object Main {
+  def padString(toPad: String, paddedLength: Int): String = {
+    if(paddedLength <= toPad.length)
+      return toPad
+    padString(toPad+" ", paddedLength)
+  }
+
+  def printFormatted(T: Any, V: Any, N: Any, RS: Any, MinV: Any, MaxV: Any) : Unit = {
+    println(padString(T.toString, 11) + padString(V.toString, 9) + padString(N.toString, 3) + padString(RS.toString, 9) + padString(MinV.toString, 9) + padString(MaxV.toString, 9))
+  }
+
+  def toFiveDecimals(valueToRound: Double): Double = {
+    "%.5f".format(valueToRound).toDouble
+  }
+
+  def analyzeSequence(sequence: Seq[(Int, Double)], timeWindow: Int): Unit = {
+    val (startTime, value) = sequence.head
+    val filteredSequence = sequence.filter(x => x._1 < (startTime+timeWindow)).map(x => x._2)
+    val n = filteredSequence.length
+    val rs = filteredSequence.sum
+    val minV = filteredSequence.min
+    val maxV = filteredSequence.max
+    printFormatted(startTime, value, n, toFiveDecimals(rs), minV,maxV)
+  }
+
+  def main(args: Array[String]): Unit = {
+    printFormatted("T", "V", "N", "Rs", "MinV", "MaxV")
+
+    var slidingWindow = 60
+    if(args.length == 1) try{
+      slidingWindow = args(0).toInt
+    } catch{
+      case _: Throwable =>
+    }
+
+    Source.fromFile("./data_scala.txt")
+      .getLines
+      .map(x => x.trim.split("\\s+"))
+      .map(e => (e(0).toInt,e(1).toDouble))
+      .sliding(20, 1)
+      .foreach(x => analyzeSequence(x, slidingWindow))
+  }
+}
