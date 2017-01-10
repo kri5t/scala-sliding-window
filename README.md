cdd18a6878d4b3e1c349726e26ff1dcad308fded
Kristian Barrett <kbarrett-86@hotmail.com>
2017-01-10 23:07:18 +0100

    * Added some more read * Added fault checking and args (—window, —filepath, -w, -f)

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ A sliding window analysis of lines in a file (stream)
 * Outputs the time, price ratios, number of measurements (in current 60s), rolling sum of the window, min value and max value
 
 I have made an assumption that the 20 lines will not exceed the number of lines in the current 60s window.
+
+The program tries to parse the input. If it cannot parse col1 to int and col2 to double the program ignores this line.
+
+####Compiling:
+
+scalac ./src/Main.scala -d {outputdir}
+
+####Run:
+
+{outputdir}/scala Main --window 60 --filepath {path_to_file}
+
+If no parameters are defined it defaults to:
+* --window = 60
+* --filepath = ./data_scala.txt
--- src/Main.scala
+++ src/Main.scala
@@ -10,6 +10,16 @@ object Main {
     padString(toPad+" ", paddedLength)
   }
 
+  def safeStringToInt(str: String): Option[Int] = {
+    import scala.util.control.Exception._
+    catching(classOf[NumberFormatException]) opt str.toInt
+  }
+
+  def safeStringToDouble(str: String): Option[Double] = {
+    import scala.util.control.Exception._
+    catching(classOf[NumberFormatException]) opt str.toDouble
+  }
+
   def printFormatted(T: Any, V: Any, N: Any, RS: Any, MinV: Any, MaxV: Any) : Unit = {
     println(padString(T.toString, 11) + padString(V.toString, 9) + padString(N.toString, 3) + padString(RS.toString, 9) + padString(MinV.toString, 9) + padString(MaxV.toString, 9))
   }
@@ -28,27 +38,40 @@ object Main {
     printFormatted(startTime, value, n, toFiveDecimals(rs), minV,maxV)
   }
 
+  def tryParseValues (e: Array[String]): (Int, Double) = {
+    val time = safeStringToInt(e(0))
+    val priceRatio = safeStringToDouble(e(1))
+    if(time.isDefined && priceRatio.isDefined)
+      (time.get, priceRatio.get)
+    else
+      null
+  }
+
   def analyzeLines(linesIterator: Iterator[String], slidingWindow: Int) = {
     printFormatted("T", "V", "N", "Rs", "MinV", "MaxV")
 
     linesIterator
       .map(x => x.trim.split("\\s+"))
-      .map(e => (e(0).toInt,e(1).toDouble))
+      .map(tryParseValues)
+      .filter(x => x != null)
       .sliding(20, 1)
       .foreach(x => analyzeSequence(x, slidingWindow))
   }
 
   def main(args: Array[String]): Unit = {
-    val linesIterator = Source.fromFile("./data_scala.txt")
-      .getLines
+    var window = 60
+    var filePath = "./data_scala.txt"
 
-    var slidingWindow = 60
-    if(args.length == 1) try{
-      slidingWindow = args(0).toInt
-    } catch{
-      case _: Throwable =>
+    args.sliding(2, 1).toList.collect {
+      case Array("--window", argWindow: String) => window = argWindow.toInt
+      case Array("-w", argWindow: String) => window = argWindow.toInt
+      case Array("--filepath", argFilePath: String) => filePath = argFilePath
+      case Array("-f", argFilePath: String) => filePath = argFilePath
     }
 
-    analyzeLines(linesIterator, slidingWindow)
+    val linesIterator = Source.fromFile(filePath)
+      .getLines
+
+    analyzeLines(linesIterator, window)
   }
 }
